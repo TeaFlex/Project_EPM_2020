c03e35a0254ad25741ef724981113b0d6611f0e7
MissingNoShiny <larcinvincent@gmail.com>
2020-10-21 11:05:05 +0200

    Test CommissionClassification

--- EPM-core/src/test/java/be/heh/std/epm/domain/EmployeeTest.java
+++ EPM-core/src/test/java/be/heh/std/epm/domain/EmployeeTest.java
@@ -67,6 +67,26 @@ public class EmployeeTest {
     }
 
     @Test
+    public void createCommissionEmployee() {
+        employee.setPaymentClassification(new CommissionClassification(1000, 0.05));
+        employee.setPaymentSchedule(new MonthlyPaymentSchedule());
+
+        LocalDate date = LocalDate.of(2019, 10, 1);
+        LocalDate nextDate = LocalDate.of(2019, 10, 2);
+        LocalDate dateOutside = LocalDate.of(2019, 9, 2);
+
+        CommissionClassification classification = (CommissionClassification) employee.getPaymentClassification();
+        classification.addReceipt(new Receipt(date, 500));
+        classification.addReceipt(new Receipt(nextDate, 800));
+        classification.addReceipt(new Receipt(dateOutside, 600));
+
+        employee.payDay(pc);
+        double pay = pc.getSalary();
+
+        assertEquals(1065.0, pay, 0.01);
+    }
+
+    @Test
     public void monthlyPaymentSchedule() {
         employee.setPaymentClassification(new SalariedClassification(1000));
         employee.setPaymentMethod(new DirectDepositMethod("ING", "be80-4444-444"));
