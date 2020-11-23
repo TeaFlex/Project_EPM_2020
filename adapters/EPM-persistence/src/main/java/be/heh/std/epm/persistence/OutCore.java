fc44776f7bea1c76d7dac4445f942a4ee75cd485
teaflex <nicolas.jeuniaux.pro@hotmail.com>
2020-11-23 18:41:48 +0100

    Liens entre les modules ajouté

---
+++ adapters/EPM-persistence/src/main/java/be/heh/std/epm/persistence/OutCore.java
@@ -0,0 +1,5 @@
+package be.heh.std.epm.persistence;
+
+import be.heh.std.epm.application.port.out.OutPersistence;
+public class OutCore {
+}
---
+++ adapters/EPM-web/src/main/java/be/heh/std/epm/web/InCore.java
@@ -0,0 +1,7 @@
+package be.heh.std.epm.web;
+
+import be.heh.std.epm.application.port.in.InUseCase;
+
+public class InCore implements InUseCase {
+
+}
