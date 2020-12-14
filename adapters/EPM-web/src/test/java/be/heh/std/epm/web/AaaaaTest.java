04b5300489508cc37af3cfab42ccb39ef3ac5408
teaflex <nicolas.jeuniaux.pro@hotmail.com>
2020-12-14 14:43:28 +0100

    Test problématique

--- adapters/EPM-web/src/test/java/be/heh/std/epm/web/AaaaaTest.java
+++
@@ -1,7 +0,0 @@
-package be.heh.std.epm.web;
-
-import junit.framework.TestCase;
-
-public class AaaaaTest extends TestCase {
-
-}
---
+++ adapters/EPM-web/src/test/java/be/heh/std/epm/web/WebTest.java
@@ -0,0 +1,17 @@
+package be.heh.std.epm.web;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test1() {
+
+    }
+}
