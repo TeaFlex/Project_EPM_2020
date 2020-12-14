bd0d8e82414dc2209a3d53da059701f9306d478b
teaflex <nicolas.jeuniaux.pro@hotmail.com>
2020-12-14 14:38:40 +0100

    Suppression DEFINITVE de gson

--- adapters/EPM-web/src/main/java/be/heh/std/epm/web/ErrorResponseEntity.java
+++ adapters/EPM-web/src/main/java/be/heh/std/epm/web/ErrorResponseEntity.java
@@ -1,7 +1,5 @@
 package be.heh.std.epm.web;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -9,17 +7,12 @@ import java.sql.Timestamp;
 
 public class ErrorResponseEntity extends ResponseEntity<String> {
 
-    private static Gson gson = new Gson();
-
     public ErrorResponseEntity(String message) {
         super(generateJson(message), HttpStatus.BAD_REQUEST);
     }
 
     private static String generateJson(String message) {
-        JsonObject jsonObject = new JsonObject();
         Timestamp now = new Timestamp(System.currentTimeMillis());
-        jsonObject.addProperty("timestamp", now.toString());
-        jsonObject.addProperty("error", message);
-        return gson.toJson(jsonObject);
+        return String.format("{ \"timestamp\":\"%s\", \"error\":\"%s\" }", now.toString(), message);
     }
 }
