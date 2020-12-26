f0226c6869b4b7ffb5a075ba995adef07a0d446e
Vincent Larcin <larcinvincent@gmail.com>
2020-12-26 17:12:34 +0100

    urls de modification ?!  je crois

--- adapters/EPM-web/src/main/java/be/heh/std/epm/web/EmployeeController.java
+++ adapters/EPM-web/src/main/java/be/heh/std/epm/web/EmployeeController.java
@@ -29,31 +29,31 @@ public class EmployeeController {
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping(value = "/employee/commissioned", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/employee/commissioned", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> addCommissionedEmployee(@RequestBody AddCommissionEmployee addCommissionEmployee) {
         return executeWriteOperation(addCommissionEmployee);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping(value = "/employee/hourly", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/employee/hourly", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> addHourlyEmployee(@RequestBody AddHourlyEmployee addHourlyEmployee) {
         return executeWriteOperation(addHourlyEmployee);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping(value = "/employee/salaried", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/employee/salaried", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> addSalariedEmployee(@RequestBody AddSalariedEmployee addSalariedEmployee) {
         return executeWriteOperation(addSalariedEmployee);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @DeleteMapping(value = "/employee", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/employee", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> deleteEmployee(@RequestBody DeleteEmployee deleteEmployee) {
         return executeWriteOperation(deleteEmployee);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping(value = "/timecard", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/timecard", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> postTimeCard(@RequestBody PostTimeCard postTimeCard) {
         return executeWriteOperation(postTimeCard);
     }
@@ -62,6 +62,48 @@ public class EmployeeController {
     @PostMapping(value = "/salesreceipt", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> postSalesReceipt(@RequestBody PostReceipt postReceipt) {
         return executeWriteOperation(postReceipt);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/type/commissioned", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateToCommissioned(@RequestBody UpdateToCommissioned updateToCommissioned) {
+        return executeWriteOperation(updateToCommissioned);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/type/hourly", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateToHourly(@RequestBody UpdateToHourly updateToHourly) {
+        return executeWriteOperation(updateToHourly);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/type/salaried", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateToSalaried(@RequestBody UpdateToSalaried updateToSalaried) {
+        return executeWriteOperation(updateToSalaried);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/method/direct", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateToDirectDepositMethod(@RequestBody UpdateToDirectDepositMethod updateToDirectDepositMethod) {
+        return executeWriteOperation(updateToDirectDepositMethod);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/method/mail", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateToMailMethod(@RequestBody UpdateToMailMethod updateToMailMethod) {
+        return executeWriteOperation(updateToMailMethod);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/field/address", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateAddress(@RequestBody UpdateAddress updateAddress) {
+        return executeWriteOperation(updateAddress);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(value = "/employee/field/name", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> updateName(@RequestBody UpdateName updateName) {
+        return executeWriteOperation(updateName);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
